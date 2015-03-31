999326e6081f1949bef740cedb3e2a8a2045f07f
Youjin Shin <meggonagul@gmail.com>
2015-03-31 14:47:19 -0400

    edit

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,7 @@
+// console.log($( window ).width()); // change by window size
+// console.log(screen.width); // original screen size
+// console.log($( "body" ).width());
 
+
+
+// console.log($('.container').css('right'));
