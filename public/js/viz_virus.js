729bed2edfeceb949325696fa06a4750c8b88a4e
Youjin Shin <meggonagul@gmail.com>
2015-04-01 18:31:40 -0400

    edit

--- public/js/viz_virus.js
+++ public/js/viz_virus.js
@@ -16,7 +16,7 @@ svg2.append('rect')
 	.attr('height', height2 + margin2.top + margin2.bottom)
 	.style('fill', '#14202D');
 
-var g = svg2.append('g')
+var g2 = svg2.append('g')
 	.attr('transform', 'translate('+ margin2.left +','+ margin2.top +')');
 
 var tooltip = d3.select('body')
@@ -25,11 +25,11 @@ var tooltip = d3.select('body')
 
 var xScale = d3.scale.linear()
 	.domain([0, 70])
-	.range([10, width2 - 10]);
+	.range([80, width2 - 30]);
 
 var yScale = d3.scale.linear()
-	.domain([0, getLogValue(23816)])
-	.range([height2 - 100, 20]);
+	.domain([getLogValue(1), getLogValue(23816)])
+	.range([height2 - 100, 60]);
 
 queue()
 	.defer(d3.csv, 'viruses.csv')
@@ -37,7 +37,10 @@ queue()
 
 function ready(error, viruses) {
 
-	lines = g.selectAll('.line')
+	getGuideVirus();
+	getGuideHost();
+
+	lines = g2.selectAll('.line')
 			.data(viruses)
 				.enter()
 			.append('line')
@@ -68,7 +71,7 @@ function ready(error, viruses) {
 				d3.select(this).style('opacity', 0.22);
 			});
 
-	dots = g.selectAll('.dot')
+	dots = g2.selectAll('.dot')
 			.data(viruses)
 				.enter()
 			.append('circle')
@@ -111,22 +114,22 @@ d3.select(".host")
 
 function updateByCount() {
 
-		lines.transition().duration(430)
-			.attr('x1', function(d) { return xScale(d.Order); })
-			.attr('x2', function(d) { return xScale(d.Order); });
+	lines.transition().duration(430)
+		.attr('x1', function(d) { return xScale(d.Order); })
+		.attr('x2', function(d) { return xScale(d.Order); });
 
-		dots.transition().duration(430)
-			.attr('cx', function(d) { return xScale(d.Order); });
+	dots.transition().duration(430)
+		.attr('cx', function(d) { return xScale(d.Order); });
 }
 
 function updateByHost() {
 
-		lines.transition().duration(430)
-			.attr('x1', function(d, i) { return xScale(i); })
-			.attr('x2', function(d, i) { return xScale(i); });
+	lines.transition().duration(430)
+		.attr('x1', function(d, i) { return xScale(i); })
+		.attr('x2', function(d, i) { return xScale(i); });
 
-		dots.transition().duration(430)
-			.attr('cx', function(d, i) { return xScale(i); });
+	dots.transition().duration(430)
+		.attr('cx', function(d, i) { return xScale(i); });
 }
 
 function getLogValue(d) {
@@ -136,19 +139,107 @@ function getLogValue(d) {
 
 function getColorVirus(d) {
 
-	// if(d == 'Animal') { return '#F06493'; }
-	// else if(d == 'Bacteria') { return '#92A7B4'; }
-	// // else if(d == 'Bacteria') { return '#C34CD7'; }
-	// else if(d == 'Plant') { return '#6AB8F7'; }
-	// else { return '#00E3CD'; }
-
-	// var colorLists = ['#F44336', '#E91E63', '#9C27B0', '#3F51B5'
-
 	if(d == 'Animal') { return '#6ab8f7'; } // blue
-	else if(d == 'Bacteria') { return '#92A7B4'; } // gray
+	else if(d == 'Bacteria') { return 'rgba(255,255,255,0.86)'; } // white
+	// else if(d == 'Bacteria') { return '#92A7B4'; } // gray
 	else if(d == 'Plant') { return '#00E3CD'; }  // green: 00E3CD, FF916F
 	else { return '#F8877F'; } // #F8877F ,  80C883
 }
 
+var lineValues = [1, 10, 100, 1000, 10000, 100000];
 
+function getGuideVirus() {
 
+	for(var i = 0; i < lineValues.length; i++) {
+
+		var r = lineValues[i];
+
+		g2.append('line')
+			.attr('x1', xScale(0) )
+			.attr('y1', yScale( getLogValue(r) ) )
+			.attr('x2', xScale(70) )
+			.attr('y2', yScale( getLogValue(r) ) )
+			.attr('stroke', '#92A7B4')
+			.attr('stroke-width', 0.5)
+			.style("stroke-dasharray", ("1,4"));
+
+		g2.append('text')
+			.attr('class', 'scaleText')
+			.attr('x',xScale(0) - 10)
+			.attr('y', yScale( getLogValue(r) ) )
+			.text(r)
+			.style('opacity', 1)
+			.attr('stroke-width', 1)
+			.style("text-anchor", "end");
+	}
+}
+
+function getGuideHost() {
+
+	// line
+	line_bacteria = g2.append('line')
+			.attr('x1', xScale(0))
+			.attr('x2', xScale(35))
+			.attr('y1', yScale(0) + 30)
+			.attr('y2', yScale(0) + 30)
+			.attr('stroke', 'rgba(255,255,255,0.86)')
+			.attr('stroke-width', 1.2);
+
+	line_animal = g2.append('line')
+			.attr('x1', xScale(36))
+			.attr('x2', xScale(64))
+			.attr('y1', yScale(0) + 30)
+			.attr('y2', yScale(0) + 30)
+			.attr('stroke', '#6ab8f7')
+			.attr('stroke-width', 1.2);
+
+	line_plant = g2.append('line')
+			.attr('x1', xScale(65))
+			.attr('x2', xScale(67))
+			.attr('y1', yScale(0) + 30)
+			.attr('y2', yScale(0) + 30)
+			.attr('stroke', '#00E3CD')
+			.attr('stroke-width', 1.2);
+
+	line_unknown = g2.append('line')
+			.attr('x1', xScale(68))
+			.attr('x2', xScale(70))
+			.attr('y1', yScale(0) + 30)
+			.attr('y2', yScale(0) + 30)
+			.attr('stroke', '#F8877F')
+			.attr('stroke-width', 1.2);
+
+	// text
+	text_bacteria = g2.append('text')
+			.attr('class', 'virusHost')
+			.attr('x', xScale(0))
+			.attr('y', yScale(0) + 50)
+			.text('Bacteria')
+			.style('fill', 'rgba(255,255,255,0.86)')
+			.style("text-anchor", "start");
+
+	text_animal = g2.append('text')
+			.attr('class', 'virusHost')
+			.attr('x', xScale(36))
+			.attr('y', yScale(0) + 50)
+			.text('Animal')
+			.style('fill', '#6ab8f7')
+			.style("text-anchor", "start");
+
+	text_plant = g2.append('text')
+			.attr('class', 'virusHost')
+			.attr('x', xScale(65))
+			.attr('y', yScale(0) + 50)
+			.text('Plant')
+			.style('fill', '#00E3CD')
+			.style("text-anchor", "start");
+
+	text_unknown = g2.append('text')
+			.attr('class', 'virusHost')
+			.attr('x', xScale(68))
+			.attr('y', yScale(0) + 50)
+			.text('Unknown')
+			.style('fill', '#F8877F')
+			.style("text-anchor", "start");
+
+}
